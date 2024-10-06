e7d255b0d7327c6d3d2ec533835ec0cc4fa5b6f3
cereal <autonomouscereal@gmail.com>
2024-10-06 12:25:07 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import Signup from './Signup';
+import Callback from './callback';
+import Dashboard from './Dashboard';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const pathname = window.location.pathname;
+
+  if (pathname === '/callback') {
+    return <Callback />;
+  } else if (pathname === '/dashboard') {
+    return <Dashboard />;
+  } else {
+    return <Signup />;
+  }
 }
 
 export default App;
---
+++ src/Dashboard.js
@@ -0,0 +1,14 @@
+// src/Dashboard.js
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>You have successfully logged in!</p>
+      {/* Add your dashboard content here */}
+    </div>
+  );
+}
+
+export default Dashboard;
